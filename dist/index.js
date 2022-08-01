c4d6664a4783b84b57de5fabe3a8c5a017528934
Enricky Hipólito <enricky.hipolito@gmail.com>
2022-07-31 23:47:09 -0300

    export types

--- dist/index.js
+++ dist/index.js
@@ -1,50 +1,37 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-const createSecondHalf_1 = __importDefault(require("./createSecondHalf"));
-const easy_shuffle_1 = __importDefault(require("easy-shuffle"));
-/**
- * Genereates a Round-Robin schedule.
- * @param array Array of teams or elements to schedule.
- * @param repeat If true, it will generate a second half of the same matches generated, but with reversed home. It will be included in the returned array of rounds.
- * @returns An array with every created round.
- */
-function roundrobin(array, repeat) {
-    const rounds = [];
-    let elementsNumber = array.length;
-    // this allow the creation of rounds with odd number of teams
-    if (elementsNumber % 2 === 1) {
-        array.push(null);
-        elementsNumber++;
-    }
-    for (let round = 0; round < elementsNumber - 1; round++) {
-        rounds[round] = [];
-        for (let i = 0; i < elementsNumber / 2; i++) {
-            const other = elementsNumber - i - 1;
-            const team1 = array[i];
-            const team2 = array[other];
-            if (team1 !== null && team2 !== null) {
-                const match = [team1, team2];
-                if (round % 2 === 1 && i === 0)
-                    match.reverse();
-                rounds[round].push(match);
-            }
-            rounds[round] = (0, easy_shuffle_1.default)(rounds[round]); //randomize the order of the matches in the round
+var __createBinding =
+  (this && this.__createBinding) ||
+  (Object.create
+    ? function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        var desc = Object.getOwnPropertyDescriptor(m, k);
+        if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+          desc = {
+            enumerable: true,
+            get: function () {
+              return m[k];
+            },
+          };
         }
-        //* these next lines changes the position of the last element to the second (index 1) element of the array.
-        const lastElement = array.pop();
-        if (lastElement !== undefined) {
-            array.splice(1, 0, lastElement);
-        }
-    }
-    const shuffledRounds = (0, easy_shuffle_1.default)(rounds); //randomize the order of the rounds to prevent many matches away or home.
-    // create the second half
-    if (repeat) {
-        const secondHalf = (0, createSecondHalf_1.default)(shuffledRounds);
-        shuffledRounds.push(...secondHalf);
-    }
-    return shuffledRounds;
-}
-module.exports = roundrobin;
+        Object.defineProperty(o, k2, desc);
+      }
+    : function (o, m, k, k2) {
+        if (k2 === undefined) k2 = k;
+        o[k2] = m[k];
+      });
+var __exportStar =
+  (this && this.__exportStar) ||
+  function (m, exports) {
+    for (var p in m)
+      if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, "__esModule", { value: true });
+const roundrobin_1 = __importDefault(require("./roundrobin"));
+module.exports = roundrobin_1.default;
+__exportStar(require("./types"), exports);
 //# sourceMappingURL=index.js.map
